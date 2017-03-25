da043f347c9fb411a2865abfa741b25304c9fc4a
Ye Yan <haskell.ye.yan@gmail.com>
2017-03-25 16:43:24 +1100

    add implementation

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Wrap matrix in a R flavour Class
 
 makeCacheMatrix <- function(x = matrix()) {
+    ## initialize solve to NULL
+    s <- NULL
+    set <- function(y) {
+        x <<- y
+        s <<- NULL ## when reset matrix, inverse is reset as well 
+    }
+    get <- function() x
+    setsolve <- function(solve) s <<- solve
+    getsolve <- function() s
 
+    ## return R flavour class
+    list(set = set, get = get,
+         getsolve = getsolve,
+         setsolve = setsolve)
 }
 
-
-## Write a short comment describing this function
+## Calculate inverse matrix for Object that produced by makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    s <- x$getsolve()
+    if(!is.null(s)) {
+        message("getting cached data")
+        return(s)
+    }
+    data <- x$get()
+    s <- solve(data, ...)
+    x$setsolve(s)
+    s
 }
